6cb0b0c41b620330df2d971766b2ad7ab78b7047
sergio2509 <sergio2509@gmail.com>
2018-11-03 16:10:49 -0400

    Models added
    se añadieron los modelos de land currecy language regional blocks translation usando el postman y el jason to c#

---
+++ Lands2/Lands2/Models/Currency.cs
@@ -0,0 +1,27 @@
+﻿namespace Lands2.Models
+{
+   using Newtonsoft.Json;
+
+   class Currency
+   {
+
+      [JsonProperty(PropertyName = "code")]
+      public string Code
+      {
+         get; set;
+      }
+
+      [JsonProperty(PropertyName = "name")]
+      public string Name
+      {
+         get; set;
+      }
+
+      [JsonProperty(PropertyName = "symbol")]
+      public string Symbol
+      {
+         get; set;
+      }
+   }
+
+}
---
+++ Lands2/Lands2/Models/RegionalBloc.cs
@@ -0,0 +1,22 @@
+﻿namespace Lands2.Models
+{
+   using Newtonsoft.Json;
+
+   class RegionalBloc
+   {
+
+      [JsonProperty(PropertyName = "acronym")]
+      public string Acronym
+      {
+         get; set;
+      }
+
+      [JsonProperty(PropertyName = "name")]
+      public string Name
+      {
+         get; set;
+      }
+
+          
+   }
+}
