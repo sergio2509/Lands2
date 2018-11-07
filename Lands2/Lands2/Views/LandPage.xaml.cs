b6f463480a168456dc237d847c8910ed68f28a89
sergio2509 <Sergei@MacBook-Pro.local>
2018-11-06 20:05:06 -0400

    LandView y LandViewModel
    se inicio el trabajo en este view es la clase 14 al medio

---
+++ Lands2/Lands2/Views/LandPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Lands2.Views
+{
+    public partial class LandPage : ContentPage
+    {
+        public LandPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
